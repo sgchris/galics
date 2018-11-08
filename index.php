5a4e04e04382c9d21c0a95b06c9048dc00f32819
Gregory Chris <sgchris@gmail.com>
2018-11-08 18:35:25 +0200

    added "magnific popup". added breadcrumbs

--- index.php
+++ index.php
@@ -7,13 +7,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="magnific-popup.css">
     <link rel="stylesheet" href="styles.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="jquery.magnific-popup.min.js"></script>
+    <script>
+        $(function() {
+            $('.card-file').magnificPopup({
+                type: 'image',
+                gallery:{
+                    enabled:true
+                }
+                // other options
+                // ..
+            });
+        });
+    </script>
 </head>
 <body>
+
+<?php 
+$dir = __DIR__;
+if (isset($_GET['dir'])) {
+    if (is_dir(__DIR__.'/'.$_GET['dir'])) {
+        $dir = __DIR__.'/'.$_GET['dir'];
+    }
+}
+
+$relativeDir = trim(str_replace(__DIR__, '', $dir), '/');
+?>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <a class="navbar-brand" href="#">
@@ -22,17 +47,25 @@
     </a>
 </nav>
 
-<div class="container-fluid main-wrapper">
-    <div class="row"><div class="col-12">
-    <?php 
-    $dir = __DIR__;
-    if (isset($_GET['dir'])) {
-        if (is_dir(__DIR__.'/'.$_GET['dir'])) {
-            $dir = __DIR__.'/'.$_GET['dir'];
-        }
-    }
-    ?>
+<!-- breadcrumpbs -->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item" aria-current="page"><a href="?">Home</a></li>
+        <?php $folderParts = explode('/', $relativeDir); ?>
+        <?php foreach ($folderParts as $i => $folderItem) { ?>
+        <li class="breadcrumb-item" aria-current="page">
+            <?php if ($i == count($folderParts) - 1) { ?>
+            <?php echo $folderItem; ?>
+            <?php } else { ?>
+            <a href="?dir=<?php echo implode('/', array_slice($folderParts, 0, $i + 1)); ?>"><?php echo $folderItem; ?></a>
+            <?php } ?>
+        </li>
+        <?php } ?>
+    </ol>
+</nav>
 
+<div class="container-fluid main-wrapper">
+    <div class="row"><div class="col-12 main-row">
     <?php
     // display folders
     foreach (glob($dir.'/*', GLOB_ONLYDIR) as $folder) {
@@ -45,7 +78,7 @@
                     <?php
                     // check if has folders
                     if (count(glob($folder.'/*', GLOB_ONLYDIR)) > 0) {
-                        ?><div class="preview-thumb-wrapper"><i class="far fa-folder-open"></i></div><?php
+                        ?><div class="preview-thumb-wrapper"><i class="far fa-folder"></i></div><?php
                     }
                     
                     $imageFiles = glob($folder.'/*.jpg');
